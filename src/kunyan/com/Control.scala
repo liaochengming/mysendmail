5f675d5ae933f53052a6118488c00933e2cf07f5
liaochengming <liaochengming@kunyan-inc.com>
2017-09-20 11:27:00 +0800

    Initial commit

---
+++ src/kunyan/com/Control.scala
@@ -0,0 +1,15 @@
+package kunyan.com
+
+/**
+ * Created by lcm on 2017/6/5.
+ * 发送邮件
+ */
+object Control {
+
+  def main(args: Array[String]) {
+    //MySendMail.sendMail("测试主题","数据异常报警测试")
+    //val file = new File("D:\\D\\jar包\\javamail-1.4.5\\mail.jar")
+    //println(file.length())
+
+  }
+}
